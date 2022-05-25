abd77a84534aa8e710cfae9a814ca677f1f8d86c
Shyamu Yadav <shyamu431@protonmail.ch>
2022-05-25 17:25:37 +0530

    fix: Some change

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 
-x_values = [1,2,3,4,5]
-y_values = [1,4,9,16,25]
+x_values = range(1,1001)
+y_values = [x**2 for x in range(1,1001)]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s = 100)
+
+ax.scatter(x_values, y_values, s = 10)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
@@ -14,5 +15,7 @@ ax.set_ylabel("Square of Value", fontsize=14)
 
 # Set size of tick labels.
 ax.tick_params(axis='both',which='major', labelsize=14)
+
+ax.axis([0, 110, 0, 1100000])
 
 plt.show()
